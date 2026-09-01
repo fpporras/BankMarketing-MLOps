b2a9ec5d23265b6897da81f3d3e1d577f9bcd67f
Fanny Sandoval <fsandovalp@ucenfotec.ac.cr>
2026-09-01 02:43:58 -0600

    orchestrator.py
    Archivos modificados para ser ejecutados solamente por el orquestador

--- src/features/prepare_data.py
+++ src/features/prepare_data.py
@@ -1,72 +1,42 @@
 from pathlib import Path
-
-from src.ingestion.ingest import load_raw_data
 from src.features.build_features import build_features
-
-
-# ============================================================
-# RUTAS
-# ============================================================
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 PROCESSED_DIR = (
-    PROJECT_ROOT
-    / "data"
-    / "processed"
+    PROJECT_ROOT /
+    "data" /
+    "processed"
 )
 
 PROCESSED_FILE = (
-    PROCESSED_DIR
-    / "df_features.csv"
+    PROCESSED_DIR /
+    "df_features.csv"
 )
 
 
-# ============================================================
-# PREPARACIÓN
-# ============================================================
-
-def prepare_processed_data():
+def prepare_processed_data(df_raw):
 
     print("=" * 70)
-    print("PREPARANDO DATASET PROCESADO")
+    print("ETAPA — FEATURE ENGINEERING")
     print("=" * 70)
-
-    # --------------------------------------------------------
-    # Crear directorio
-    # --------------------------------------------------------
 
     PROCESSED_DIR.mkdir(
         parents=True,
         exist_ok=True
     )
 
-    # --------------------------------------------------------
-    # Cargar RAW
-    # --------------------------------------------------------
-
-    df_raw = load_raw_data()
-
     print(
         f"Dataset RAW: {df_raw.shape}"
     )
-
-    # --------------------------------------------------------
-    # Feature Engineering
-    # --------------------------------------------------------
 
     df_processed = build_features(
         df_raw
     )
 
     print(
-        f"Dataset PROCESSED: "
-        f"{df_processed.shape}"
+        f"Dataset PROCESSED: {df_processed.shape}"
     )
-
-    # --------------------------------------------------------
-    # Guardar
-    # --------------------------------------------------------
 
     df_processed.to_csv(
         PROCESSED_FILE,
@@ -74,17 +44,8 @@ def prepare_processed_data():
     )
 
     print(
-        f"\nDataset procesado guardado en:\n"
+        f"Dataset procesado guardado en:\n"
         f"{PROCESSED_FILE}"
     )
 
     return df_processed
-
-
-# ============================================================
-# MAIN
-# ============================================================
-
-if __name__ == "__main__":
-
-    prepare_processed_data()
